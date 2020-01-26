814b9e417e1598ff6c37e2bda4d3439ca9283cf9
Paulius Kutka <pauliuskutka@Pauliuss-MacBook-Pro.local>
2020-01-26 20:42:31 +0000

    Vue tut

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <Header />
+    <router-view />
+  </div>
+</template>
+
+<script>
+import Header from "./components/layout/Header";
+export default {
+  name: "App",
+  components: {
+    Header
+  }
+};
+</script>
+<style>
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
---
+++ src/components/AddTodo.vue
@@ -0,0 +1,40 @@
+<template>
+  <form @submit.prevent="addTodo">
+    <input type="text" v-model="title" name="title" placeholder="Add Todo..." />
+    <input type="submit" name="Submit" class="btn" />
+  </form>
+</template>
+
+<script>
+export default {
+  name: "AddTodo",
+  data() {
+    return {
+      title: ""
+    };
+  },
+  methods: {
+    addTodo() {
+      const newTodo = {
+        name: this.title,
+        completed: false
+      };
+      this.$emit("add-todo", newTodo);
+      this.title = "";
+    }
+  }
+};
+</script>
+
+<style scoped>
+form {
+  display: flex;
+}
+input[type="text"] {
+  flex: 10;
+  padding: 5px;
+}
+input[type="submit"] {
+  flex: 2;
+}
+</style>
